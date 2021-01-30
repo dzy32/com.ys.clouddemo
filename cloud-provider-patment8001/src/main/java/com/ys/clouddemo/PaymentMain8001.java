e90c4c1f6b7a1ac97a3ca2853abcb39582029bf8
ys <1107857837@qq.com>
2021-01-30 10:08:39 +0800

    1临时保存解决冲突

--- cloud-provider-patment8001/src/main/java/com/ys/clouddemo/PaymentMain8001.java
+++ cloud-provider-patment8001/src/main/java/com/ys/clouddemo/PaymentMain8001.java
@@ -1,11 +1,14 @@
 package com.ys.clouddemo;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.context.annotation.Bean;
 
 /**
  * @author ys
@@ -22,5 +25,14 @@ public class PaymentMain8001 {
     public static void main(String[] args) {
 
         SpringApplication.run(PaymentMain8001.class,args);
+    }
+    @Bean
+    public ServletRegistrationBean getServlet(){
+        HystrixMetricsStreamServlet hystrix=new HystrixMetricsStreamServlet();
+        ServletRegistrationBean bean=new ServletRegistrationBean(hystrix);
+        bean.setLoadOnStartup(1);
+        bean.addUrlMappings("/hystrix.stream");
+        bean.setName("HystrixMetricsStreamServlet");
+        return bean;
     }
 }
